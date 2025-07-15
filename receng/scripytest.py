981bf35c03332ae918620465cce55f3c2d976f03
ketchupspill <sean.sun231@gmail.com>
2025-07-15 12:50:31 +0800

    commit beginning of recAI

---
+++ receng/scripytest.py
@@ -0,0 +1,32 @@
+# from scipy import spatial
+
+# a = [1, 2]
+# b = [2, 4]
+# c = [2.5, 4]
+# d = [4.5, 5]
+
+# print(spatial.distance.euclidean(c, a))
+
+# # 2.5
+# spatial.distance.euclidean(c, b)
+# # 0.5
+# spatial.distance.euclidean(c, d)
+# # 2.23606797749979
+
+# >>> from scipy import spatial
+# >>> a = [1, 2]
+# >>> b = [2, 4]
+# >>> c = [2.5, 4]
+# >>> d = [4.5, 5]
+
+# >>> spatial.distance.cosine(c,a)
+# 0.004504527406047898
+
+# >>> spatial.distance.cosine(c,b)
+# 0.004504527406047898
+
+# >>> spatial.distance.cosine(c,d)
+# 0.015137225946083022
+
+# >>> spatial.distance.cosine(a,b)
+# 0.0
